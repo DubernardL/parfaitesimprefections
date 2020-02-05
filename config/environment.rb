00df7950e1ecce227518363637a7cb69b83ea30b
Lucas Dubernard <lucas.dubernard@hotmail.fr>
2020-02-05 14:07:29 +0100

    error 2

--- config/environment.rb
+++ config/environment.rb
@@ -4,15 +4,3 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.default_url_options = { host: 'https://parfaites-imperfections.herokuapp.com/' }
-config.action_mailer.delivery_method = :smtp
-
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'heroku.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 465,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
