8e7e773c492490377404232ed78f92f54a76fe2f
Lucas Dubernard <lucas.dubernard@hotmail.fr>
2020-02-05 14:45:46 +0100

    herokuapp.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,13 +92,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config.action_mailer.default_url_options = { host: 'https://parfaites-imperfections.herokuapp.com/' }
-  # config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'https://parfaites-imperfections.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password => ENV["SENDGRID_PASSWORD"],
+    :domain => 'herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 465,
     :authentication => :plain,
