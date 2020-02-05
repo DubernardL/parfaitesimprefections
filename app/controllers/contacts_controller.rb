43edcf4c279708a9b57d3e03ee1e363585638488
Lucas Dubernard <lucas.dubernard@hotmail.fr>
2020-02-05 17:20:38 +0100

    form contact added

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,16 +3,4 @@ class ContactsController < ApplicationController
     @categories = Category.all
     @contact = Contact.new
   end
-
-  def create
-    @categories = Category.all
-    @contact = Contact.new(params[:contact])
-
-    if @contact.valid?
-      Mailer.contact_form(@contact).deliver
-      redirect_to new_contact_path, flash: {success: t(:"create.message_has_been_sent")}
-    else
-      render :new
-    end
-  end
 end
