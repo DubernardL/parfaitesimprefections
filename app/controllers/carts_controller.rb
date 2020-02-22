73a1eb44a2363e6c3665fcf63d16b4657a338897
Lucas Dubernard <lucas.dubernard@hotmail.fr>
2020-02-22 17:58:02 +0100

    CSS cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,5 +4,11 @@ class CartsController < ApplicationController
 
   def show
     @categories = Category.all
+
+    @total = 0
+    @cart.cart_items.each do |cart_item|
+      total_item = cart_item.item.price * cart_item.quantity
+      @total += total_item
+    end
   end
 end
